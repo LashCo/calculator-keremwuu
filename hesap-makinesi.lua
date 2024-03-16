fa3b808badf82638ef3010ff2edc0808a1988535
Keremwuuu <herseyyazilim@gmail.com>
2024-03-16 17:49:36 +0300

    Create hesap-makinesi.lua

---
+++ hesap-makinesi.lua
@@ -0,0 +1,27 @@
+--burası toplama yapmak için
+print("keremwuu calculator")
+print("toplama yapmak için 1.sayı:")
+sayi1 = tonumber(io.read())
+print("toplama yapmak için 2.sayı:")
+sayi2 = tonumber(io.read())
+sonuc = sayi1 + sayi2
+print("Sonuç: " .. sonuc)
+------------------------------------------------
+--bu bölümde çıkarma yapıcağız
+print("keremwuu calculator")
+print("çıkarma yapmak için 1.sayı:")
+sayi1 = tonumber(io.read())
+print("çıkarma yapmak için 2.sayı:")
+sayi2 = tonumber(io.read())
+sonuc = sayi1 - sayi2
+print("Sonuç: " .. sonuc)
+------------------------------------------------
+--bu bölümde çarpma işlemi yapıyoruz
+print("keremwuu calculator")
+print("çarpma yapmak için 1.sayı:")
+sayi1 = tonumber(io.read())
+print("çarpma yapmak için 2.sayı:")
+sayi2 = tonumber(io.read())
+sonuc = sayi1 * sayi2
+print("Sonuç: " .. sonuc)
+--------------------------------------------------
